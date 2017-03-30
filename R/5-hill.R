76c919f07dc57d866111e3018e61e3d2e5c76bca
Haziq Jamil <haziqjamil@users.noreply.github.com>
2017-03-30 20:44:48 +0100

    Hill valley data

--- R/5-hill.R
+++ R/5-hill.R
@@ -2,19 +2,11 @@
 source("R/1-gpr-iprior-sim-functions.R")
 
 experiment.name <- "Hill-valley data"
-# (https://archive.ics.uci.edu/ml/datasets/Arrhythmia) This database contains
-# 279 attributes, 206 of which are linear valued and the rest are nominal.
-# Concerning the study of H. Altay Guvenir: "The aim is to distinguish between
-# the presence and absence of cardiac arrhythmia and to classify it in one of
-# the 16 groups. Class 01 refers to 'normal' ECG classes 02 to 15 refers to
-# different classes of arrhythmia and class 16 refers to the rest of
-# unclassified ones. For the time being, there exists a computer program that
-# makes such a classification. However there are differences between the
-# cardiolog's and the programs classification. Taking the cardiolog's as a gold
-# standard we aim to minimise this difference by means of machine learning
-# tools."
-# Binary classification task: "normal (0)" or "arrhythmia (1)" p = 100 (ECG
-# measurements), N = 451
+# The hill–valley data set (http://archive.ics.uci.edu/ml/datasets/Hill-Valley)
+# consists of 1212 observations of a terrain, each when plotted in sequence
+# represents either a hill (class 0; size 600) or a valley (class 1; size 612).
+# The task is to classify the terrain on the basis of a vector of dimension p =
+# 100.
 load("data/Hill.RData")
 summary(as.factor(HillData$y))
 X.orig <- HillData$x
@@ -40,14 +32,14 @@ tab <- tabRes("GPR (linear)"            = res.gprlin,
               "I-prior (FBM MLE)"       = res.ipfbmoptim)
 
 # Results from REC
-rp.lda5.mean <- c(13.05, 10.75, 9.78)
-rp.lda5.se   <- c(0.38, 0.25, 0.26)
+rp.lda5.mean <- c(36.84, 36.45, 32.57)
+rp.lda5.se   <- c(0.84, 0.85, 1.06)
 rp.lda5      <- meanAndSE(rp.lda5.mean, rp.lda5.se)
-rp.qda5.mean <- c(8.14, 6.15, 5.21)
-rp.qda5.se   <- c(0.37, 0.37, 0.20)
+rp.qda5.mean <- c(44.43, 43.56, 41.10)
+rp.qda5.se   <- c(0.34, 0.31, 0.33)
 rp.qda5      <- meanAndSE(rp.qda5.mean, rp.qda5.se)
-rp.knn5.mean <- c(13.05, 7.43, 5.43)
-rp.knn5.se   <- c(0.46, 0.25, 0.19)
+rp.knn5.mean <- c(49.08, 47.27, 366.39)
+rp.knn5.se   <- c(0.24, 0.26, 0.29)
 rp.knn5      <- meanAndSE(rp.knn5.mean, rp.knn5.se)
 rp.tab <- rbind("RP5-LDA" = rp.lda5, "RP5-QDA" = rp.qda5, "RP5-knn" = rp.knn5)
 colnames(rp.tab) <- colnames(tab$tab)
